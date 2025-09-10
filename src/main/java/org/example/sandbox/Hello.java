f5614e3061f9640d8dbeb1b65382c867c6f6ee96
Spyro1001 <raymondhedgecock@gmail.com>
2025-09-09 19:30:43 -0700

    adding inheritance

--- src/main/java/org/example/sandbox/Hello.java
+++ src/main/java/org/example/sandbox/Hello.java
@@ -3,6 +3,8 @@ package org.example.sandbox;
 public class Hello {
 
     public static void main(String[] args) {
+
+        Hello hello = new Hello();
         System.out.println("Hello, World!");
     }
 
