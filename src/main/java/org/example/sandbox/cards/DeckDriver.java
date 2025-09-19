053eb45eafe50e85cde9973cf503c88080bc37c2
Spyro1001 <raymondhedgecock@gmail.com>
2025-09-18 19:31:27 -0700

    adding rest of cards

--- src/main/java/org/example/sandbox/cards/DeckDriver.java
+++ src/main/java/org/example/sandbox/cards/DeckDriver.java
@@ -5,5 +5,18 @@ public class DeckDriver {
     public static void main(String[] args) {
         Deck deck = new StandardDeck();
         System.out.println("Deck created: " + deck);
+
+        //shuffle the deck
+        deck.shuffle();
+        System.out.println("Deck shuffled: " + deck);
+
+        //cut the deck
+        deck.cut(26);
+        System.out.println("Deck cut: " + deck);
+
+        //deal a card
+        Card dealtCard = deck.dealCard();
+        System.out.println("Dealt card: " + dealtCard);
+        System.out.println("Deck after dealing a card: " + deck);
     }
 }
